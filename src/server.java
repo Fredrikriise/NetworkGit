6579fe96b75e82fb4c05c0cd78fde203a7a5aba9
Ocianus <fredrik1998@hotmail.com>
2019-01-29 16:16:06 +0100

    Push 2 med tid

--- src/server.java
+++ src/server.java
@@ -1,7 +1,6 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.PrintStream;
@@ -24,48 +23,30 @@ public class server {
        PrintStream p = new PrintStream(ss.getOutputStream());
        p.println(temp);
 
-        String search = "https://www.google.com/search?q=" + temp + "time";
+        String search = "https://www.google.com/search?q=" + temp + "+" + "time";
         SimpleDateFormat date_format=new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
         java.util.Date date = new Date();
         String worldTime = date_format.format(date);
 
-       Document doc = Jsoup.connect(search).get();
-       System.out.println(doc.title());
-       //Elements newsHeadlines = doc.select("div.gsrt vk_bk dDoNo");
-       Elements currentTime = doc.select("div.gsrt vk_bk dDoNo");
-       for(Element time : currentTime) {
-           System.out.println("%s\n\t%s" + time.attr("title") + time.absUrl("href"));
-       }
-
-
-       /*
-       URL url = new URL("https://www.worldtimeserver.com");
-       HttpURLConnection con = (HttpURLConnection) url.openConnection();
-       con.setRequestMethod("GET");
-
-        Map<String, String> parameters = new HashMap<>();
-        parameters.put("by", by);
-
-        con.setDoOutput(true);
-        DataOutputStream out = new DataOutputStream(con.getOutputStream());
-        out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
-        out.flush();
-        out.close();
-
-
-        con.setRequestProperty("User-Agent", "World Time Server");
-
-        int status = con.getResponseCode();
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(search).get();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        in.close();
-        con.disconnect();
-*/
+        System.out.println(doc.title());
+
+        Element contentDiv = doc.select("div[class=gsrt vk_bk dDoNo]").first();
+
+        if(contentDiv == null) {
+            System.out.println("Kunne ikke funne lokaltid til " + temp);
+           // return "Kunne ikke funne lokaltid";
+        }
+
+        String text = contentDiv.getElementsByTag("div").text();
+        System.out.println(text);
+
+
     }
 
 }
